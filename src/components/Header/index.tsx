30335d935a7cabdbf7c35473b04336a7b7322aa6
Alef Ligiero <alefligieroagb@gmail.com>
2023-09-19 21:44:20 -0300

    header finished

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,33 @@
+import { NavLink } from "react-router-dom";
 import "./styles.css";
+import { Link } from "react-router-dom";
+import homeImg from "../../assets/home.svg"
 
 export default function Header() {
     return (
-        <header></header>
+        <header className="header">
+            <nav className="navbar site-container">
+                <div className="navbar-items">
+                    <div className="navbar-item">
+                        <NavLink className={({ isActive }) => (isActive ? "menu-active" : "")} to={"/"}>
+                            Início
+                        </NavLink>
+                    </div>
+                    <div className="navbar-item">
+                        <NavLink className={({ isActive }) => (isActive ? "menu-active" : "")} to={"/products"}>
+                            Produtos
+                        </NavLink>
+                    </div>
+                    <div className="navbar-item">
+                        <NavLink className={({ isActive }) => (isActive ? "menu-active" : "")} to={"/about"}>
+                            Sobre nós
+                        </NavLink>
+                    </div>
+                </div>
+                <Link to={"/"}>
+                    <img src={homeImg} alt="Home" />
+                </Link>
+            </nav>
+        </header>
     );
 }
